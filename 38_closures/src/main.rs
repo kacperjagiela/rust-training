a166a99a849eb51efd9ecda3a53127accd6c7760
kacperjagiela <jagielakacper80@gmail.com>
2024-03-30 21:46:30 +0100

    feat: 38_closures impl

--- 38_closures/src/main.rs
+++ 38_closures/src/main.rs
@@ -1,3 +1,122 @@
+use std::{thread, time::Duration, vec};
+
+#[derive(Debug, Clone, Copy)]
+
+enum ShirtColor {
+    Red,
+    Blue,
+}
+
+struct Inventory {
+    shirts: Vec<ShirtColor>,
+}
+
+impl Inventory {
+    fn giveaway(&self, user_preference: Option<ShirtColor>) -> ShirtColor {
+        user_preference.unwrap_or_else(|| self.most_stocked())
+    }
+
+    fn most_stocked(&self) -> ShirtColor {
+        let mut num_red = 0;
+        let mut num_blue = 0;
+
+        for color in &self.shirts {
+            match color {
+                ShirtColor::Red => num_red += 1,
+                ShirtColor::Blue => num_blue += 1,
+            }
+        }
+
+        if num_red > num_blue {
+            ShirtColor::Red
+        } else {
+            ShirtColor::Blue
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let store = Inventory {
+        shirts: vec![ShirtColor::Blue, ShirtColor::Red, ShirtColor::Blue],
+    };
+
+    let user_pref1 = Some(ShirtColor::Red);
+
+    let giveaway1 = store.giveaway(user_pref1);
+
+    println!(
+        "The user with preference {:?} gets {:?}",
+        user_pref1, giveaway1
+    );
+
+    let user_pref2 = None;
+    let giveaway2 = store.giveaway(user_pref2);
+    println!(
+        "The user with prefecnce {:?} gets {:?}",
+        user_pref2, giveaway2
+    );
+
+    let expensive_closure = |num: u32| -> u32 {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    };
+
+    // example of type infer -> gets first type from usage
+
+    let example_closure = |x| x;
+
+    //let s = example_closure(String::from("hello"));
+    let n = example_closure(5);
+
+    // capturing references
+
+    let mut list = vec![1, 2, 3];
+
+    println!("Before defining closure: {:?}", list);
+
+    let mut only_borrows = || list.push(7);
+
+    only_borrows();
+    println!("After calling closure: {:?}", list);
+
+    // moving ownership with "move"
+
+    let list = vec![1, 2, 3];
+
+    println!("Before defining closure: {:?}", list);
+
+    thread::spawn(move || println!("From thread: {:?}", list))
+        .join()
+        .unwrap();
+
+    #[derive(Debug)]
+    struct Rectangle {
+        width: u32,
+        height: u32,
+    }
+
+    let mut list = [
+        Rectangle {
+            width: 10,
+            height: 1,
+        },
+        Rectangle {
+            width: 3,
+            height: 5,
+        },
+        Rectangle {
+            width: 7,
+            height: 12,
+        },
+    ];
+
+    let mut num_sort_operations = 0;
+
+    list.sort_by_key(|rect| {
+        num_sort_operations += 1;
+        rect.width
+    });
+
+    println!("{:#?}, sorted in {num_sort_operations} operations", list);
 }
