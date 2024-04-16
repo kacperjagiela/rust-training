24d4da9e05861810373841ed314f38f36c772c1b
kacperjagiela <jagielakacper80@gmail.com>
2024-04-16 16:34:22 +0200

    feat: 55_implementing_oop_pattern

--- 55_implementing_oop_pattern/src/main.rs
+++ 55_implementing_oop_pattern/src/main.rs
@@ -1,3 +1,14 @@
+use implementing_oop_pattern::Post;
+
 fn main() {
-    println!("Hello, world!");
+    let mut post = Post::new();
+
+    post.add_text("I ate a salad for lunch");
+    assert_eq!("", post.content());
+
+    post.request_review();
+    assert_eq!("", post.content());
+
+    post.approve();
+    assert_eq!("I ate a salad for lunch", post.content());
 }
